684a4200a227b3b29f61303e632bb22035fa9cf5
shubham-kumar50 <imshubhamkr50@gmail.com>
2021-05-23 21:51:00 +0530

    Adding the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanImage from './images/alan.png';
+import useStyles from './styles.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const alanKey = '5e1a5784bdb4aab4fbf5186cc70b744e2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+    const [newsArticles, setnewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+
+    useEffect(() => {
+        alanBtn({
+            key: '5e1a5784bdb4aab4fbf5186cc70b744e2e956eca572e1d8b807a3e2338fdd0dc/stage',
+            onCommand: ({command, articles, number, article, savedArticles}) => {
+                if(command === 'newHeadlines'){
+                    setnewsArticles(articles);
+                    setActiveArticle(-1);
+                }
+                else if (command === 'highlight') {
+                    //for(let start = 0;start<savedArticles.length;start++){
+                        setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                        //alanBtn().playText(`${savedArticles[start].title}`);
+                    //}
+                }
+                else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+          
+                    if (parsedNumber > articles.length) {
+                      alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                      window.open(article.url, '_blank');
+                      alanBtn().playText('Opening...');
+                    } else {
+                      alanBtn().playText('Please try that again...');
+                    }
+                }
+            },
+        });
+    },[]);
+
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                {newsArticles.length ? (
+                <div className={classes.infoContainer}>
+                    <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+                    <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+                </div>
+                ) : null}
+                <img src={alanImage} className={classes.alanLogo} alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        </div>
+    );
 }
 
 export default App;
